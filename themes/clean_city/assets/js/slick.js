513462c1fa00ffe637e74171cecdaa48981c4d08
jasper-kollinz <adyl.syubaev@gmail.com>
2021-06-11 13:41:40 +0300

    slider

--- themes/clean_city/assets/js/slick.js
+++ themes/clean_city/assets/js/slick.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
     $('.image-data').slick({
-      
+      arrows: false,
+      dots: false,
+      adaptiveHeight: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
     });
   });
