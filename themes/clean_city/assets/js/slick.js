ccba4bf574c30b320b227b761c6493b8f7698840
gadimlie <ceo@gadimlie.com>
2021-06-11 18:13:06 +0400

    fixes

---
+++ themes/clean_city/assets/js/slick.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.image-data').slick({
+       arrows: false,
+       dots: false,
+       adaptiveHeight: true,
+       infinite: true,
+       autoplay: true,
+       autoplaySpeed: 3000,
+       speed: 1000,
+    });
+ });
